355f04f696e4b9f2936bb41e7b16cb5d74b79e23
danieleverrienti86 <danieleverrienti1986@gmail.com>
2021-11-18 23:38:13 +0100

    Country microservice first commit

---
+++ cyclingapp-backend/coutrymicrosevice/src/main/java/com/cyclingapp/coutrymicrosevice/CoutrymicroseviceApplication.java
@@ -0,0 +1,13 @@
+package com.cyclingapp.coutrymicrosevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoutrymicroseviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoutrymicroseviceApplication.class, args);
+	}
+
+}
