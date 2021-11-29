bb4fbb5d5c44d1367574c4951943267eb906152f
danieleverrienti86 <danieleverrienti1986@gmail.com>
2021-11-29 22:37:00 +0100

    Initialize docker db configuration

---
+++ docker-compose/cycling_app_data.sql
@@ -0,0 +1,3 @@
+insert into divisions (division_name, position) values('Continental', 3);
+insert into divisions (division_name, position) values('Professional', 2);
+insert into divisions (division_name, position) values('Word Tour', 1);
---
+++ docker-compose/cycling_app_schema.sql
@@ -0,0 +1,13 @@
+create table if not exists countries (
+ 	id BIGINT UNIQUE AUTO_INCREMENT,
+    country_code VARCHAR(8) NOT NULL,
+    country_name VARCHAR(80) NOT NULL,
+    PRIMARY KEY (id)
+)
+
+create table if not exists divisions(
+	id BIGINT UNIQUE AUTO_INCREMENT,
+    division_name VARCHAR(32) NOT NULL,
+    position BIGINT NOT NULL,
+    PRIMARY KEY(id)
+)
