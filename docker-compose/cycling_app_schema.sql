df7436817199f78c0f69f2915a3c4f8720b3d540
danieleverrienti86 <danieleverrienti1986@gmail.com>
2022-01-31 17:10:16 +0100

    SQL Schema from procyclingmanager game

--- docker-compose/cycling_app_schema.sql
+++ docker-compose/cycling_app_schema.sql
@@ -2,7 +2,6 @@ create table if not exists countries (
  	id BIGINT UNIQUE AUTO_INCREMENT,
     country_code VARCHAR(8) NOT NULL,
     country_name VARCHAR(80) NOT NULL,
-    code VARCHAR(8) NOT NULL,
     PRIMARY KEY (id)
 )
 
@@ -13,12 +12,23 @@ create table if not exists divisions(
     PRIMARY KEY(id)
 )
 
-create table if not exists cyclists(
-  	id   BIGINT AUTO_INCREMENT UNIQUE,
-    first_name VARCHAR(80) NOT NULL,
-    last_name VARCHAR(80) NOT NULL,
-    BORN DATE NOT NULL,
-    country_id BIGINT NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (country_id) REFERENCES countries(id)    
+CREATE TABLE IF NOT EXISTS team (
+   id              BIGINT  NOT NULL PRIMARY KEY AUTO_INCREMENT
+  ,team_name       VARCHAR(64) NOT NULL
+  ,general_manager VARCHAR(64)
+  ,country_id      BIGINT  NOT NULL
+  ,id_division     BIGINT  NOT NULL
+  ,FOREIGN KEY (country_id) REFERENCES countries(id) 
+  ,FOREIGN KEY (id_division) REFERENCES divisions(id) 
+);
+
+CREATE TABLE IF NOT EXISTS cyclist (
+   id         BIGINT  NOT NULL PRIMARY KEY AUTO_INCREMENT
+  ,first_name VARCHAR(64) NOT NULL
+  ,last_name  VARCHAR(64) NOT NULL
+  ,born       DATE  NOT NULL
+  ,team_id    BIGINT  NOT NULL
+  ,country_id BIGINT  NOT NULL
+  ,FOREIGN KEY (country_id) REFERENCES countries(id)  
+  ,FOREIGN KEY (team_id) REFERENCES team(id) 
 );
